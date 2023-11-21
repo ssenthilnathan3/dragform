6f8d9b56a980f73baf8625ef807099e04bf60e69
Senthilnathan <ssenthil1490@outlook.com>
2023-11-21 09:18:30 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import DesignerContextProvider from "@/components/context/DesignerContext";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DragForm",
+  description: "just drag and drop to create your form",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <NextTopLoader />
+          <DesignerContextProvider>
+            <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </DesignerContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
